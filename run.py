d5b5a808e82c2d7be88f13bc0c371bcea6976cc3
joshu-inf <jsibanda407@gmail.com>
2025-10-08 01:44:48 +0200

    fixed port mapping

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 import uvicorn
+import os
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "main:app",   # path to your FastAPI app
-        host="0.0.0.0",
-        port=8080,
-        reload=True       # optional, useful for development
-    )
+    port = int(os.environ.get("PORT", 8080))  # ✅ correct
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
